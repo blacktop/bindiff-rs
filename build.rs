3e3a9fdfdd4b2b8cad115618f45550d42d7b5b12
blacktop <blacktop@users.noreply.github.com>
2025-01-18 13:08:46 -0700

    chore: reorg

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() -> std::io::Result<()> {
     prost_build::Config::new()
         .default_package_filename("binexport")
-        .compile_protos(&["binexport/binexport2.proto"], &["binexport/"])?;
+        .compile_protos(&["src/binexport/binexport2.proto"], &["src/binexport/"])?;
     Ok(())
 }
