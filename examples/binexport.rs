aa146be1598e513fc212e209b25057f73465e0a7
blacktop <blacktop@users.noreply.github.com>
2025-01-18 16:51:40 -0700

    Update binexport.rs

--- examples/binexport.rs
+++ examples/binexport.rs
@@ -1,8 +1,13 @@
 use bindiff_rs::BinExport;
 
-fn main() {
-    let binexport = BinExport::open("tests/kernel.release.t6020.BinExport").unwrap();
-    for function_match in binexport. {
-        println!("{:#?}", function_match);
-    }
+fn main() -> anyhow::Result<()> {
+    let input_path = std::env::args()
+        .nth(1)
+        .ok_or_else(|| anyhow::anyhow!("Please provide a path to a BinExport file"))?;
+    
+    let binexport = BinExport::open(input_path)?;
+    
+    println!("executable_name: {}", binexport.executable_name()?);
+
+    Ok(())
 }
