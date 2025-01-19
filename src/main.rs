5bae151ba124a490c40d09ea08f1252d4b9d766b
blacktop <blacktop@users.noreply.github.com>
2025-01-18 18:17:45 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,39 @@
-use calm_io::*;
 use bindiff_rs::BinDiff;
+use calm_io::*;
 use scopeguard::guard;
 
 #[pipefail]
 fn main() -> std::io::Result<()> {
-    let input_path = std::env::args()
-        .nth(1)
-        .ok_or_else(|| std::io::Error::new(std::io::ErrorKind::InvalidInput, "Please provide a path to a BinDiff file"))?;
-    
+    let input_path = std::env::args().nth(1).ok_or_else(|| {
+        std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Please provide a path to a BinDiff file",
+        )
+    })?;
+
     let bd = BinDiff::open(&input_path)
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
 
     let bd = guard(bd, |bd| {
-        bd.close().unwrap_or_else(|e| eprintln!("Error closing database: {}", e));
+        bd.close()
+            .unwrap_or_else(|e| eprintln!("Error closing database: {}", e));
     });
 
     // Read file
-    let file = bd.read_file()
+    let file = bd
+        .read_file()
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
     stdoutln!("{}", file)?;
 
     // Read metadata
-    let metadata = bd.read_metadata()
+    let metadata = bd
+        .read_metadata()
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
     stdoutln!("{}", metadata)?;
 
     // Read function matches
-    let func_matches = bd.read_function_matches()
+    let func_matches = bd
+        .read_function_matches()
         .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
     for func_match in func_matches {
         stdoutln!("{}", func_match)?;
